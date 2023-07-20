bfb8a4b9925d9971987ae4754ea7b941cee10ede
joyoungkyoung <dughks1130@gmail.com>
2023-07-20 12:56:51 +0900

    add: next-videos

--- next.config.js
+++ next.config.js
@@ -1,22 +1,9 @@
+const withVideos = require('next-videos');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    const prefix = config.assetPrefix ?? config.basePath ?? '';
-
-    config.module.rules.push({
-      test: /\.(mov|mp4)$/,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {
-            publicPath: `${prefix}/_next/static/media/`,
-            outputPath: `${isServer ? '../' : ''}static/media/`,
-            name: '[name].[ext]',
-          },
-        },
-      ],
-    });
+  webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
@@ -25,4 +12,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withVideos(nextConfig);
